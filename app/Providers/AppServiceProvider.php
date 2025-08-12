76b45671fc907e1b239f09073dcca0b19d407b89
Sebastien48 <coriansebastien@gmail.vom>
2025-08-12 21:16:38 +0000

    ajout du providerspour définir partour les vues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
+use App\Models\Agence;
+use App\Models\User;
+
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +25,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // conserver le nom de l'agence , le logo de l'agence  et  les initialisateurs  de l'utilisateurs  connecté
+        View::composer('layout', function ($view) {
+        $user = Auth::user();
+        $agence = Agence::where('user_id', Auth::id())->first();
+        // Obtenir les initiales : première lettre du nom et première lettre du prénom
+        $initiales = strtoupper(substr($user->name, 0, 1) . substr($user->prenom, 0, 1));
+        // Importer le logo de l'agence via le modèle Agence
+        $logo = $agence->logo ?? asset('images/default-logo.png');
+        // Passer les données à la vue
+        $view->with([
+            'agenceName'   => $agence->name ?? 'Agence',
+            'agenceLogo'   => $logo,
+            'userInitials' => $initiales,
+        ]);
+    });
+            
+        
     }
 }
